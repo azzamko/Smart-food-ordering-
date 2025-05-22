55ba0184f98076fdef8ed40e0b37d33e0ce5f6b8
azzamko <aliazzam221@hotmail.de>
2025-05-22 13:37:34 +0200

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Smart Food Ordering 🍔</h1>
+      <p>This site supports Arabic, English, and German!</p>
+    </div>
+  );
+}
