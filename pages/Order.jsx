707277ed6d48036e3e01d1df32aa6568a515b62f
azzamko <aliazzam221@hotmail.de>
2025-05-22 14:50:29 +0200

    Create Order.jsx

---
+++ pages/Order.jsx
@@ -0,0 +1,12 @@
+// locales/en/common.json
+{
+  "pizza": "Pizza",
+  "burger": "Burger",
+  "salad": "Salad",
+  "place_order": "Place Your Order",
+  "select_dish": "Select a dish",
+  "choose": "Choose...",
+  "quantity": "Quantity",
+  "submit_order": "Submit Order",
+  "order_received": "Order received"
+}
